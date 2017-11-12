c07b7e5ea69ece7bd5b085f42b534b0d4143259a
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-11-12 16:38:29 +0100

    feat(rest): DELETE /users/me/token
    Add new Rest API to do logout removing de auth token from the array of tokens of the user.
    Add test cases to probe it. for platforms users.

--- server/server.js
+++ server/server.js
@@ -143,6 +143,16 @@ app.post('/users/login', (req, res) => {
     });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res
+        .status(200).send();
+    }).catch(() => {
+        res
+        .status(400).send();
+    });
+});
+
 app.listen(port, () => console.log(`Express Server started at port ${port}`));
 
 module.exports = {
