4a7371f2c52b2365a1f5456a02dcad23acdb3128
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-11-12 15:18:23 +0100

    test(rest): POST /users/login
    Create test cases that probe that the Rest Method of login works fine.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -268,3 +268,53 @@ describe('POST /users', () => {
         .end(done);
     });
 });
+
+describe('POST /users/login', () =>{
+    it('Should login user and return auth token', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[1].email,
+            password: users[1].password
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.headers['x-auth']).not.toBeNull();
+        })
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+            
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens[0]).not.toBeUndefined();
+                expect(user.tokens[0].access).toBe('auth');
+                expect(user.tokens[0].token).toBe(res.headers['x-auth']);
+                done();
+            }).catch((err) => done(err));
+        });
+    });
+
+    it('Should reject invalid login', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[1].email,
+            password: users[1].password+'123'
+        })
+        .expect(400)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toBeUndefined();
+        })
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+            
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((err) => done(err));
+        });
+    });
+});
