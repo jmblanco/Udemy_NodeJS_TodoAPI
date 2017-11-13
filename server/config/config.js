1f4fdfecbe00cbd9829ea11040e9f9a283ac8da0
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-11-13 23:23:25 +0100

    build(config): Add ENV variables by JSON
    Include all the config for development and test in one JSON with this format:
    {
        "enviroment": {
            "PORT": XXXX,
            "MONGODB_URI": "XXXXX",
            "JWT_SECRET": "XXXXXX"
        },
    }
    
    Add the JWT_SECRET to the heroku config in order to deploy de app.

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,23 @@
 var env = process.env.NODE_ENV || 'development';
 
 console.log('*** Config for env:', env);
-if(env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if(env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+
+if (env === 'development' || env === 'test'){
+    const config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((envKey) => {
+        process.env[envKey] = envConfig[envKey];
+    });
 }
+
+// if(env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// }else if(env === 'test'){
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
 console.log('*** Config:');
 console.log('*** Server Port:', process.env.PORT);
 console.log('*** MongoDB URI:', process.env.MONGODB_URI);
